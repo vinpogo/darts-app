bd783df83046489ea2e58a73033493a73370b3dd
Dmitrii Stoilik <dmitrii.stoilik@tset.com>
2025-08-29 12:05:44 +0200

    prime

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,6 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Button from 'primevue/button'
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +8,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <Button label="qwe" />
 </template>
 
 <style scoped></style>
