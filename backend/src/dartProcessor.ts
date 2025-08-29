cd3b3eb2c050d7a41a2c3800b231050fe83ca973
Sebastian Martorell <sebalone789@gmail.com>
2025-08-29 17:12:58 +0200

    DART-1 | feat | improved counting system

--- backend/src/dartProcessor.ts
+++ backend/src/dartProcessor.ts
@@ -2,7 +2,7 @@ import { Field, ScoringAverage } from "../../shared/types";
 import { getAims, addAims } from "./db";
 
 export type DartShot = {
-  aim: Exclude<Field, "0">;
+  aim: FieldWithout0;
   hit: Field;
 };
 
@@ -10,27 +10,31 @@ export function processShotList(shots: DartShot[]) {
   shots.forEach((shot) => {
     let scoringAverage = getScoringAverage(shot.aim);
     // Update the values of the scoring average
-    const newScoringAverage = calculateScoringAverage(scoringAverage, shot);
-    addAims(newScoringAverage);
+    const newScoringAverage = calculateScoringAverage(scoringAverage.new, shot);
+    addAims({ ...scoringAverage.all, ...newScoringAverage });
   });
 }
 
-function getScoringAverage(shot: Exclude<Field, "0">): any {
+type FieldWithout0 = Exclude<Field, "0">;
+
+function getScoringAverage(shot: FieldWithout0) {
   const latestScoringAverage = getAims();
   // Get the key
-  return latestScoringAverage[shot];
+  return { all: getAims(), new: { [shot]: { ...latestScoringAverage[shot] } } };
 }
 
 function calculateScoringAverage(
   scoringAverage: ScoringAverage,
   shot: DartShot
 ): ScoringAverage {
+  const aim = String(shot.aim) as FieldWithout0;
+  const hit = String(shot.hit) as FieldWithout0;
   return {
     ...scoringAverage,
-    [shot.aim]: {
-      ...scoringAverage?.[shot.aim],
-      shots: scoringAverage?.[shot.aim]?.shots ?? 0 + 1,
-      [shot.hit]: scoringAverage?.[shot.aim]?.[shot.hit] ?? 0 + 1,
+    [aim]: {
+      ...scoringAverage?.[aim],
+      shots: (scoringAverage?.[aim]?.shots ?? 0) + 1,
+      [hit]: (scoringAverage?.[aim]?.[hit] ?? 0) + 1,
     },
   };
 }
