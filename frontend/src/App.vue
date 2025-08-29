13978be8ae1ff7288eeb9bdd4f45afac183b21e6
Amir Talic <amir.talic@tset.com>
2025-08-29 14:05:53 +0200

    add tailwind

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,38 @@
 <script setup lang="ts">
 import Button from 'primevue/button'
+import Fieldset from 'primevue/fieldset'
+import { computed } from 'vue'
+
+const suggestion1 = computed(() => {
+  return 123
+})
+const suggestion2 = computed(() => {
+  return 123
+})
+const suggestion3 = computed(() => {
+  return 123
+})
+
 </script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1 class="text-3xl font-bold underline">You did it!</h1>
+  <div>
+
+    <Fieldset legend="Dart 1">
+      {{suggestion1}}
+    </Fieldset>
+    <Fieldset legend="Dart 2">
+      {{suggestion2}}
+    </Fieldset>
+    <Fieldset legend="Dart 3">
+      {{suggestion3}}
+    </Fieldset>
+  </div>
+  
   <Button label="qwe" />
 </template>
 
-<style scoped></style>
+<style scoped>
+@import "tailwindcss";
+</style>
