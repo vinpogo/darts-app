8ce51b602e7453ed02759f62c11aabc70512371a
Sebastian Martorell <sebalone789@gmail.com>
2025-08-29 15:58:20 +0200

    DART-1 | feat | enabled DB conexion via post endpoint

--- backend/src/dartProcessor.ts
+++ backend/src/dartProcessor.ts
@@ -1,4 +1,5 @@
 import { Field, ScoringAverage } from "../../shared/types";
+import { getAims, addAims } from "./db";
 
 export type DartShot = {
   aim: Exclude<Field, "0">;
@@ -10,11 +11,14 @@ export function processShotList(shots: DartShot[]) {
     let scoringAverage = getScoringAverage(shot.aim);
     // Update the values of the scoring average
     const newScoringAverage = calculateScoringAverage(scoringAverage, shot);
+    addAims(newScoringAverage);
   });
 }
 
-function getScoringAverage(shot: Field): any {
-  return {};
+function getScoringAverage(shot: Exclude<Field, "0">): any {
+  const latestScoringAverage = getAims();
+  // Get the key
+  return latestScoringAverage[shot];
 }
 
 function calculateScoringAverage(
@@ -25,7 +29,7 @@ function calculateScoringAverage(
     ...scoringAverage,
     [shot.aim]: {
       ...scoringAverage[shot.aim],
-      shots: scoringAverage[shot.aim].shots + 1,
+      shots: scoringAverage[shot.aim]?.shots ?? 0 + 1,
       [shot.hit]: scoringAverage?.[shot.aim]?.[shot.hit] ?? 0 + 1,
     },
   };
--- backend/src/db.ts
+++ backend/src/db.ts
@@ -4,19 +4,17 @@ import { ScoringAverage, Field } from "../../shared/types";
 export const db = new Database("averages.sqlite", { create: true });
 
 export function addAims(scoringAverage: ScoringAverage) {
-    const stmt = db.prepare(
-  "INSERT INTO aims (json) VALUES (?)"
-);
-stmt.run(170, 1, JSON.stringify(scoringAverage));
+  const stmt = db.prepare("INSERT INTO aims (json) VALUES (?)");
+  stmt.run(JSON.stringify(scoringAverage));
 }
 
-export function getAims() {
+export function getAims(): ScoringAverage {
   const query = db.query<ScoringAverage[], SQLQueryBindings>(
-    "SELECT json FROM aims LIMIT 1;"
+    "SELECT json FROM aims order by id DESC LIMIT 1;"
   );
 
   // @ts-expect-error sqlite
-  console.log('query.all()', query.all());
+  console.log("query.all()", query.all());
   // @ts-expect-error sqlite
   const someString = query.all()[0].json;
   console.log(someString);
