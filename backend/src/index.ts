ffdb1123cebf341f1dda78e4471dae4a98026ac6
Florian Stradner <florian.stradner@tset.com>
2025-08-29 14:52:22 +0200

    added db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
 import { Hono } from 'hono'
 import { serveAI } from './serveAI'
-
+import { db } from './db'
 const app = new Hono()
+
+
 
 app.get('/', serveAI)
 
@@ -9,3 +11,5 @@ export default {
   port: 3521, 
   fetch: app.fetch, 
 } 
+
+db.close(false);
